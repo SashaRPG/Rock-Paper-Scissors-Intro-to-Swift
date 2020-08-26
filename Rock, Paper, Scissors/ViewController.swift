8db96d73e46df3b1491fbdce5059560e042d66d0
SashaRPG <uasasharpg@gmail.com>
2020-08-26 21:36:10 +0300

    Initial Commit

---
+++ Rock, Paper, Scissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock, Paper, Scissors
+//
+//  Created by ALEXANDER KOTLUBEEV on 26/08/2020.
+//  Copyright © 2020 ALEXANDER KOTLUBEEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
